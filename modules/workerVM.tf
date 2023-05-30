2762057518be3c205e48603d091a54fdc0a14f1f
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2023-05-30 11:37:37 +0530

    Update workerVM.tf

--- modules/workerVM.tf
+++ modules/workerVM.tf
@@ -2,7 +2,7 @@ resource "azurerm_linux_virtual_machine" "worker-vm" {
   name                = "worker-vm"
   resource_group_name = var.resource_group_name
   location            = var.location
-  size                = "Standard_DS1_v2"
+  size                = var.size
   admin_username      = "ubuntu"
 
   network_interface_ids = [
