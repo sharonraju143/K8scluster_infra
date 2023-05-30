4ab9e482e90067c2ecc177ce1e4f20d5c2043e82
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2023-05-30 11:35:41 +0530

    Update workerVM.tf

--- modules/workerVM.tf
+++ modules/workerVM.tf
@@ -1,18 +1,17 @@
 resource "azurerm_linux_virtual_machine" "worker-vm" {
   name                = "worker-vm"
-  resource_group_name =var.resource_group_name
-  location            =var.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
   size                = "Standard_DS1_v2"
   admin_username      = "ubuntu"
+
   network_interface_ids = [
     azurerm_network_interface.worker-nw-interface.id,
   ]
 
-
   admin_ssh_key {
     username   = "ubuntu"
     public_key = file("~/.ssh/id_rsa.pub")
-  }
   }
 
   os_disk {
@@ -27,4 +26,4 @@ resource "azurerm_linux_virtual_machine" "worker-vm" {
     version   = "latest"
   }
 }
-}
+
