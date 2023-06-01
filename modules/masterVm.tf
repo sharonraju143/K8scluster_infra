331f5ffd4bfe699292c201a930cf42b2ac88e229
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2023-06-01 18:14:05 +0530

    Update masterVm.tf

--- modules/masterVm.tf
+++ modules/masterVm.tf
@@ -1,5 +1,5 @@
 resource "azurerm_linux_virtual_machine" "example" {
-  name                = "master-vm"
+  name                = "k8s-master-vm"
   resource_group_name = var.resource_group_name
   location            = var.location
   size                = var.size
