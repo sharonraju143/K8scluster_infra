10f1cb356a6c82a1059bd130a246de23a98440cc
sharonraju143 <102852234+sharonraju143@users.noreply.github.com>
2023-06-01 18:17:03 +0530

    Update workerVM.tf

--- modules/workerVM.tf
+++ modules/workerVM.tf
@@ -1,5 +1,5 @@
 resource "azurerm_linux_virtual_machine" "worker-vm" {
-  name                = "-k8s-worker-vm"
+  name                = "k8s-worker-vm"
   resource_group_name = var.resource_group_name
   location            = var.location
   size                = var.size
